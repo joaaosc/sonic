5531b742065a42ae71bc2adc1742a23966643068
joaop <86691551+joaaosc@users.noreply.github.com>
2024-01-02 14:57:05 -0300

    Add main_lib module and modify existing files
    Removed openCV_Utils.py and replaced the previous main() function located in main.py to enhance usability and maintainability. Added a new python file named main_lib.py which contains the refactored code and introduced multiprocessing. Also modified pdfUtils.py to improve image cropping and text extraction mechanisms.

---
+++ ml-lib.py
@@ -0,0 +1,44 @@
+# (C) Copyright 2023 João Pedro de S. T. Costa; under Apache 2.0 License. Read EOF for more information.
+
+from transformers import DetrImageProcessor, DetrForObjectDetection
+import torch
+from PIL import Image
+import requests
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+# you can specify the revision tag if you don't want the timm dependency
+processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-101", revision="no_timm")
+model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-101", revision="no_timm")
+
+inputs = processor(images=image, return_tensors="pt")
+outputs = model(**inputs)
+
+# convert outputs (bounding boxes and class logits) to COCO API
+# let's only keep detections with score > 0.9
+target_sizes = torch.tensor([image.size[::-1]])
+results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+
+for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+    box = [round(i, 2) for i in box.tolist()]
+    print(
+            f"Detected {model.config.id2label[label.item()]} with confidence "
+            f"{round(score.item(), 3)} at location {box}"
+    )
+
+
+
+
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
--- pdfUtils.py
+++ pdfUtils.py
@@ -31,49 +31,13 @@ class Pdfutils:
             self.text = page.get_text().lower()
             print(self.text)
             if "reference drawing" in self.text or "tooling sketch" in self.text:
-                self.crop_pdf()
                 print("\033[92m[SUCCESS]: Found 'Reference Drawing' or 'Tooling Sketch'\033[0m")
                 return True
         print("\033[93m[WARN]: Didn't find 'Reference Drawing' or 'Tooling Sketch'.\033[0m")
         return None
 
     def crop_pdf(self):
-        document = self._open_document()
-        new_document = fitz.open()
-
-        for page_number in range(len(document)):
-            page = document[page_number]
-            words = page.get_text("words")
-            words.sort(key=lambda w: (w[3], w[0]))
-            found = False
-            min_x = 0
-            max_x = 0
-
-            # Get the position of "reference drawing" or "tooling sketch"
-            for word in words:
-                if 'reference drawing' in word[4].lower() or 'tooling sketch' in word[4].lower():
-                    if not found:  # check if this if the first occurrence
-                        min_x = word[0]
-                        found = True
-                    if word[2] > max_x:  # search for maximum x to cut the right part of the page too
-                        max_x = word[2]
-
-            # If the text is found
-            if found:
-                left_rect = fitz.Rect(0, 0, min_x, page.rect.height)  # rectangle for left side
-                right_rect = fitz.Rect(max_x, 0, page.rect.width, page.rect.height)  # rectangle for right side
-
-                # Add left and right part of the page to new document
-                new_page_left = new_document.new_page(width=left_rect.width, height=page.rect.height)
-                new_page_left.show_pdf_page(left_rect, page.pixmap())
-                new_page_right = new_document.new_page(width=right_rect.width, height=page.rect.height)
-                new_page_right.show_pdf_page(right_rect, page.pixmap())
-
-        document.close()
-        if new_document.page_count > 0:
-            new_document.save('/home/joaop/PycharmProjects/sonic/output.pdf')
-        else:
-            print("Warning: The new document has zero pages.")
+        pass  # todo
 
     def get_possible_drwaings(self):
         document = self._open_document()
@@ -103,10 +67,6 @@ class Pdfutils:
             img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
             arr = np.array(img)
             images.append(arr)
-
-
-
-
         if save:
             try:
                 # Get the current directory
@@ -114,8 +74,8 @@ class Pdfutils:
                 print(curr_dir)
                 # Define the full output path
                 full_path = os.path.join(curr_dir, 'combined.png')
-#               Image.fromarray(np.concatenate(images, axis=0), 'RGB').save(full_path)
-#               print(f"Image saved to {full_path}")
+                Image.fromarray(np.concatenate(images, axis=0), 'RGB').save(full_path)
+                print(f"Image saved to {full_path}")
 
                 #return True
                 return Image.fromarray(np.concatenate(images, axis=0), 'RGB')
